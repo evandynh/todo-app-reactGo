5fd46fd6602e87b4fcede3aaa4954f94fbfddede
Eric Van Dyn Hoven <eric.vandynhoven@gmail.com>
2017-05-15 14:01:32 -0700

    remove boilerplate pt 1

--- app/routes.jsx
+++ app/routes.jsx
@@ -32,8 +32,6 @@ export default (store) => {
   return (
     <Route path="/" component={App}>
       <IndexRoute component={Vote} fetchData={fetchVoteData} />
-      <Route path="login" component={LoginOrRegister} onEnter={redirectAuth} />
-      <Route path="dashboard" component={Dashboard} onEnter={requireAuth} />
       <Route path="about" component={About} />
     </Route>
   );
